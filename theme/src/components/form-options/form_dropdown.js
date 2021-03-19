24db7067b8b88436fc9cf83371a9909e2a56214f
Nelson Wei <nelson@ensemble.com>
2021-03-19 12:01:39 -0700

    Cleanup

--- theme/src/components/form-options/form_dropdown.js
+++ theme/src/components/form-options/form_dropdown.js
@@ -1,66 +1,65 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  var x, i, j, l, ll, selElmnt, a, b, c;
-  x = document.getElementsByClassName("cmp-form-options--drop-down");
-  l = x.length;
+document.addEventListener('DOMContentLoaded', function (event) {
+  var x, i, j, l, ll, selElmnt, a, b, c
+  x = document.getElementsByClassName('cmp-form-options--drop-down')
+  l = x.length
 
   for (i = 0; i < l; i++) {
-    selElmnt = x[i].getElementsByTagName("select")[0];
-    if (typeof selElmnt === "undefined")
-      return;
-    ll = selElmnt.length;
+    selElmnt = x[i].getElementsByTagName('select')[0]
+    if (typeof selElmnt === 'undefined') return
+    ll = selElmnt.length
 
     /* For each element, create a new DIV that will act as the selected item: */
-    a = document.createElement("DIV");
-    a.setAttribute("class", "select-selected");
-    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-    x[i].appendChild(a);
+    a = document.createElement('DIV')
+    a.setAttribute('class', 'select-selected')
+    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML
+    x[i].appendChild(a)
 
     /* For each element, create a new DIV that will contain the option list: */
-    b = document.createElement("DIV");
-    b.setAttribute("class", "select-items select-hide");
+    b = document.createElement('DIV')
+    b.setAttribute('class', 'select-items select-hide')
 
     for (j = 1; j < ll; j++) {
       /* For each option in the original select element,
             create a new DIV that will act as an option item: */
-      c = document.createElement("DIV");
-      c.innerHTML = selElmnt.options[j].innerHTML;
-      c.addEventListener("click", function (e) {
+      c = document.createElement('DIV')
+      c.innerHTML = selElmnt.options[j].innerHTML
+      c.addEventListener('click', function (e) {
         /* When an item is clicked, update the original select box,
                 and the selected item: */
-        var y, i, k, s, h, sl, yl;
-        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-        sl = s.length;
-        h = this.parentNode.previousSibling;
+        var y, i, k, s, h, sl, yl
+        s = this.parentNode.parentNode.getElementsByTagName('select')[0]
+        sl = s.length
+        h = this.parentNode.previousSibling
 
         for (i = 0; i < sl; i++) {
           if (s.options[i].innerHTML == this.innerHTML) {
-            s.selectedIndex = i;
-            h.innerHTML = this.innerHTML;
-            y = this.parentNode.getElementsByClassName("same-as-selected");
-            yl = y.length;
+            s.selectedIndex = i
+            h.innerHTML = this.innerHTML
+            y = this.parentNode.getElementsByClassName('same-as-selected')
+            yl = y.length
             for (k = 0; k < yl; k++) {
-              y[k].removeAttribute("class");
+              y[k].removeAttribute('class')
             }
-            this.setAttribute("class", "same-as-selected");
-            break;
+            this.setAttribute('class', 'same-as-selected')
+            break
           }
         }
-        h.click();
-      });
-      b.appendChild(c);
+        h.click()
+      })
+      b.appendChild(c)
     }
 
-    x[i].appendChild(b);
-    a.addEventListener("click", function (e) {
+    x[i].appendChild(b)
+    a.addEventListener('click', function (e) {
       /* When the select box is clicked, close any other select boxes,
             and open/close the current select box: */
-      e.stopPropagation();
-      closeAllSelect(this);
-      this.nextSibling.classList.toggle("select-hide");
-      this.classList.toggle("select-arrow-active");
-    });
+      e.stopPropagation()
+      closeAllSelect(this)
+      this.nextSibling.classList.toggle('select-hide')
+      this.classList.toggle('select-arrow-active')
+    })
   }
-});
+})
 
 function closeAllSelect(elmnt) {
   /* A function that will close all select boxes in the document,
@@ -70,25 +69,25 @@ function closeAllSelect(elmnt) {
     i,
     xl,
     yl,
-    arrNo = [];
-  x = document.getElementsByClassName("select-items");
-  y = document.getElementsByClassName("select-selected");
-  xl = x.length;
-  yl = y.length;
+    arrNo = []
+  x = document.getElementsByClassName('select-items')
+  y = document.getElementsByClassName('select-selected')
+  xl = x.length
+  yl = y.length
   for (i = 0; i < yl; i++) {
     if (elmnt == y[i]) {
-      arrNo.push(i);
+      arrNo.push(i)
     } else {
-      y[i].classList.remove("select-arrow-active");
+      y[i].classList.remove('select-arrow-active')
     }
   }
   for (i = 0; i < xl; i++) {
     if (arrNo.indexOf(i)) {
-      x[i].classList.add("select-hide");
+      x[i].classList.add('select-hide')
     }
   }
 }
 
 /* If the user clicks anywhere outside the select box,
 then close all select boxes: */
-document.addEventListener("click", closeAllSelect);
+document.addEventListener('click', closeAllSelect)
--- theme/src/components/progressbar/_progressbar.js
+++ theme/src/components/progressbar/_progressbar.js
@@ -1,18 +1,18 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  var x = document.getElementsByClassName("cmp-progressbar");
-  var targetElements = document.getElementsByClassName("progressbar");
+document.addEventListener('DOMContentLoaded', function (event) {
+  var x = document.getElementsByClassName('cmp-progressbar')
+  var targetElements = document.getElementsByClassName('progressbar')
 
-  var l = x.length;
+  var l = x.length
 
   for (var i = 0; i < l; i++) {
-    var percentage = x[i].firstElementChild.textContent;
+    var percentage = x[i].firstElementChild.textContent
 
-    var element = document.createElement("div");
-    element.setAttribute("class", "cmp-progressbar-completedPercentage");
-    var text = document.createTextNode(percentage + "%");
-    element.appendChild(text);
+    var element = document.createElement('div')
+    element.setAttribute('class', 'cmp-progressbar-completedPercentage')
+    var text = document.createTextNode(percentage + '%')
+    element.appendChild(text)
 
-    if (typeof targetElements[i] !== "undefined")
-      targetElements[i].insertBefore(element, targetElements[i].firstChild);
+    if (typeof targetElements[i] !== 'undefined')
+      targetElements[i].insertBefore(element, targetElements[i].firstChild)
   }
-});
+})
